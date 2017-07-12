1fff3c40c2f22456511bbbd06b6b98e3791f4c08
lguillain <lguillain@Leonores-MacBook-Pro.local>
2017-07-12 10:00:08 +0900

    Updated Venn diagram, statistic bar

---
+++ src/components/charts/statisticsBar.js
@@ -0,0 +1,80 @@
+//handles singular element
+class StatElement extends React.Component {
+  render() {
+    return (
+      <td>{this.props.element}</td>
+    );
+  }
+}
+
+//handles most used / least used pair
+class StatDuo extends React.Component {
+  render(){
+    return (<tr>
+      <StatElement element={this.props.mostUsed}/>
+      <StatElement element={this.props.leastUsed} />
+    </tr>);
+  }
+}
+
+//handles table content
+class StatRow extends React.Component {
+  render() {
+
+    let mostUsed = this.props.stats.mostUsed,
+       leastUsed = this.props.stats.leastUsed;
+    var rows = [];
+    //Check that datastructure is as expected
+    if(mostUsed.length != leastUsed.length) throw "Unequal Length ERROR";
+
+    for(let i = 0; i < mostUsed.length; i++){
+        rows.push(<StatDuo mostUsed={mostUsed[i]} leastUsed={leastUsed[i]} key={mostUsed[i]+leastUsed[i]} />);
+    }    
+    
+    return (
+        <tbody>
+            {rows}
+        </tbody>
+    );
+  }
+}
+
+//handles singular card
+class StatCard extends React.Component {
+  render() {
+      console.log(this.props.stats)
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Most Used</th>
+            <th>Least Used</th>
+          </tr>
+        </thead>
+        <StatRow stats={this.props.stats}/>
+      </table>
+    );
+  }
+}
+
+//Created 3 cards
+class StatisticsDeck extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.stats.map(element => <StatCard stats={element} />)}
+      </div>
+    );
+  }
+}
+
+
+var STATISTICS =[
+  {mostUsed: ["Flour", "Eggs", "Sugar"], leastUsed: ["Strawberries", "White chocolate", "Coconut Oil"]},
+  {mostUsed: ["Mix", "Bake", "Cool"], leastUsed: ["Blache", "Boil", "Flamble"]}
+  ];
+ 
+ReactDOM.render(
+  <StatisticsDeck stats={STATISTICS} />,
+  document.getElementById('container')
+);
--- src/components/charts/vennDiagram.js
+++ src/components/charts/vennDiagram.js
@@ -5,27 +5,26 @@ data = {
     Recipe2 :  {ingredients : ["a", "lot","of","ingredients"]
     },
 };
-var svg = d3.selectAll("svg");
-var width = 360,
+
+const width = 360,
     height = 400;
 
 //Constants
-var lineHeight = 30;
-var padding = 20;
-var spreadValue = .525; //servers to mak ellipse look nice
-var y = 200
+const lineHeight = 30, 
+      padding = 20, 
+      spreadValue = .525, //servers to mak ellipse look nice
+      y = 200;
 
 
 //CALCULATIONS for DIFFERENT VARIABLES
-var rx1 = width/3-padding; //devide space into 3 sections
-var rx2 = width/3-padding;
+let rx1 = width/3-padding; //devide space into 3 sections
+let rx2 = width/3-padding;
 
-var ry1 = Math.max(data.Recipe1.ingredients.length, 
+let ry1 = Math.max(data.Recipe1.ingredients.length, 
                     data.Intersection.ingredients.length+1) * lineHeight*spreadValue;
-var ry2 = Math.max(data.Recipe2.ingredients.length, 
+let ry2 = Math.max(data.Recipe2.ingredients.length, 
                     data.Intersection.ingredients.length+1) * lineHeight*spreadValue;
-var cx1 = width/3;
-var cx2 = width/3*2;
+let cx1 = width/3, cx2 = width/3*2;
 
 
 //ensure that eclipes start at same height
@@ -38,14 +37,13 @@ else {
     cy2 = y+(ry2-ry1);
 }
 
-
-var svg = d3.select("body").append("svg")
+let svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
 //HANDLE ELLIPSES
 
-var g1 = d3.selectAll("svg")
+let g1 = d3.selectAll("svg")
     .append("g");
     g1.append("ellipse")
     .attr("cx", cx1)
@@ -55,7 +53,7 @@ var g1 = d3.selectAll("svg")
     .style("fill", "brown")
     .style("fill-opacity", ".5");
     
-var g2 = svg.append("g");
+let g2 = svg.append("g");
     g2.append("ellipse")
     .attr("cx", cx2)
     .attr("cy", cy2)
