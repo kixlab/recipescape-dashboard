e27c36d5410b1462ec60bea78b0ad991fa754bed
Hyeungshik Jung <hyungshike@gmail.com>
2017-09-01 13:10:08 +0900

    Add redux devtool composer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk';
 import reducer from './reducer';
 
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk)
-);
+const store = createStore(reducer, composeWithDevTools(
+    applyMiddleware(thunk)
+));
 
 export default store;
