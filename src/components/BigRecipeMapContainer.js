55712639cdf40e2cf82830cc65a0f0443c109337
Hyeungshik Jung <hyungshike@gmail.com>
2017-09-11 20:47:29 +0900

    Display formal title of dish

--- src/components/BigRecipeMapContainer.js
+++ src/components/BigRecipeMapContainer.js
@@ -4,10 +4,21 @@ import InteractiveClusters from "../containers/InteractiveClusters"
 import InteractiveClusterSelection from "../containers/InteractiveClusterSelection"
 import { Grid, Divider,Icon } from 'semantic-ui-react'
 
+const getRealName = name => {
+    switch(name) {
+        case 'chocochip':
+            return 'Chocolate Chip Cookie'
+        case 'tomatopasta':
+            return 'Tomato Pasta'
+        default:
+            return name
+    }
+}
+
 export const BigRecipeMapContainer = ({clusters, name, changeDishname}) => (
     <div className={"RecipeMap"}>
-        <h2 type={"display3"}>RecipeMap for {name}
-                <Icon color='grey' size='mini' inverted name='settings' 
+        <h2 type={"display3"}>RecipeMap for {getRealName(name)}
+                <Icon color='grey' size='mini' inverted name='settings'
                 onClick={() => {name === 'tomatopasta'? changeDishname('chocochip'): changeDishname('tomatopasta')}}/>
         </h2>
         <Grid celled>
