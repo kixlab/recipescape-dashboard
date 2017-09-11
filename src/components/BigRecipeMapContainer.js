4b7397a6d412947647e2485d0bc7bc2a2e3ff08d
Hyeungshik Jung <hyungshike@gmail.com>
2017-09-11 21:26:35 +0900

    display3

--- src/components/BigRecipeMapContainer.js
+++ src/components/BigRecipeMapContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import InteractiveGroupByControls from "../containers/InteractiveGroupByControls"
 import InteractiveClusters from "../containers/InteractiveClusters"
 import InteractiveClusterSelection from "../containers/InteractiveClusterSelection"
-import { Grid, Divider,Icon } from 'semantic-ui-react'
+import { Dropdown, Grid, Divider,Icon } from 'semantic-ui-react'
 
 const getRealName = name => {
     switch(name) {
@@ -15,12 +15,22 @@ const getRealName = name => {
     }
 }
 
+const ClusterModeDropdown = () => (
+    <Dropdown text='Group By'>
+        <Dropdown.Menu>
+            <Dropdown.Item text="Structure" />
+            <Dropdown.Item text="Ingredients" />
+        </Dropdown.Menu>
+    </Dropdown>
+)
+
 export const BigRecipeMapContainer = ({clusters, name, changeDishname}) => (
     <div className={"RecipeMap"}>
         <h2 type={"display3"}>RecipeMap for {getRealName(name)}
                 <Icon color='grey' size='mini' inverted name='settings'
                 onClick={() => {name === 'tomatopasta'? changeDishname('chocochip'): changeDishname('tomatopasta')}}/>
         </h2>
+        <ClusterModeDropdown />
         <Grid celled>
             <Grid.Column>
                 <InteractiveClusters clusters={clusters} height={350} width={500} />
